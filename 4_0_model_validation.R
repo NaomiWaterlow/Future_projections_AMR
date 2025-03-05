c4b32c653bbf78c9a4ea3bd79fc9f97309afb306
gwenknight <eidegkni@mac-j9f3dw9939.fritz.box>
2025-03-05 13:46:09 +0000

    Updating the appendices

--- 4_0_model_validation.R
+++ 4_0_model_validation.R
@@ -12,9 +12,9 @@ coverage_23 <- data.table(read.csv("data/coverage_2023.csv"))
 
 #for storing the comparison data
 store_comparison <- data.frame()
-# NOTE - WE have more combinations in the ooriginal data that are available in the 2023 ATLAS download
-# so can't valudate everything! DOn't validate combined ones in case other things are included. 
-##### GWEN PLEASE DOUBLE CHECK DRUG MATCHING in this file!!
+# NOTE - WE have more combinations in the original data that are available in the 2023 ATLAS download
+# so can't validate everything! Don't validate combined ones in case other things are included. 
+##### Name and drug matching from here: https://www.ecdc.europa.eu/en/publications-data/tessy-metadata-report
 bug_drugs_dict <- read.csv("bug_names.csv")
 
 
@@ -25,168 +25,167 @@ bug_drugs_dict <- read.csv("bug_names.csv")
 for(i in 1:nrow(bug_drugs_dict)){
   
   for(fixed_incidence in c(T, F)){
-
- 
-# choose one bug_drug
-
-bug_choice <-bug_drugs_dict[i, "combo_name"]
-model_target <- which(plain_model_names == plain_model_names[grepl(bug_drugs_dict[i, "bug"], plain_model_names) & grepl(bug_drugs_dict[i, "drug"], plain_model_names)])
-
-validation_data_R <- validation_data[Indicator== "R - resistant isolates" & 
-                                       Population == bug_choice]
-
-coverage_23[validation_data_R, on = c(Country = "RegionName"), country_short := i.RegionCode]
-
-# load the specified model
-if(fixed_incidence == T){
-target_model <- readRDS(file = paste0("predictions/individual_predictionsfixed_",
-                                      plain_model_names[model_target], ".RDS"))
-} else if (fixed_incidence == F){
-  target_model <- readRDS(file = paste0("predictions/individual_predictionsvarying_",
-                                        plain_model_names[model_target], ".RDS"))
-}
-#take out of the storage list
-prediction_data <- target_model[[1]]
-predictions_plain <- target_model[[2]]
-predictions_age <- target_model[[3]]
-
-# combine data and predictions
-prediction_data_plain <- data.table(cbind(prediction_data, predictions_plain))
-prediction_data_age <- data.table(cbind(prediction_data, predictions_age))
-
-######## AGE MODEL ######
-#melt and combine accross age and sex
-prediction_data_age_2023 <- prediction_data_age[year_s+2009 == 2023]
-prediction_data_age_2023[, c("year_s", "age_s","age_group", "age_squared_s", "sex", "pathogen", "total", "variable", "value") := NULL]
-prediction_data_age_2023 <- melt(prediction_data_age_2023, id.vars = c("country", "projection"))
-prediction_data_age_2023 <- prediction_data_age_2023[, sum(.SD), by = c("country", "projection", "variable")]
-
-#work out range
-pred_age_2023 <- prediction_data_age_2023[, quantile(V1, probs= c(0.025, 0.5, 0.975), na.rm=T),
+    
+    
+    # choose one bug_drug
+    
+    bug_choice <-bug_drugs_dict[i, "combo_name"]
+    model_target <- which(plain_model_names == plain_model_names[grepl(bug_drugs_dict[i, "bug"], plain_model_names) & grepl(bug_drugs_dict[i, "drug"], plain_model_names)])
+    
+    validation_data_R <- validation_data[Indicator== "R - resistant isolates" & 
+                                           Population == bug_choice]
+    
+    coverage_23[validation_data_R, on = c(Country = "RegionName"), country_short := i.RegionCode]
+    
+    # load the specified model
+    if(fixed_incidence == T){
+      target_model <- readRDS(file = paste0("predictions/individual_predictionsfixed_",
+                                            plain_model_names[model_target], ".RDS"))
+    } else if (fixed_incidence == F){
+      target_model <- readRDS(file = paste0("predictions/individual_predictionsvarying_",
+                                            plain_model_names[model_target], ".RDS"))
+    }
+    #take out of the storage list
+    prediction_data <- target_model[[1]]
+    predictions_plain <- target_model[[2]]
+    predictions_age <- target_model[[3]]
+    
+    # combine data and predictions
+    prediction_data_plain <- data.table(cbind(prediction_data, predictions_plain))
+    prediction_data_age <- data.table(cbind(prediction_data, predictions_age))
+    
+    ######## AGE MODEL ######
+    #melt and combine accross age and sex
+    prediction_data_age_2023 <- prediction_data_age[year_s+2009 == 2023]
+    prediction_data_age_2023[, c("year_s", "age_s","age_group", "age_squared_s", "sex", "pathogen", "total", "variable", "value") := NULL]
+    prediction_data_age_2023 <- melt(prediction_data_age_2023, id.vars = c("country", "projection"))
+    prediction_data_age_2023 <- prediction_data_age_2023[, sum(.SD), by = c("country", "projection", "variable")]
+    
+    #work out range
+    pred_age_2023 <- prediction_data_age_2023[, quantile(V1, probs= c(0.025, 0.5, 0.975), na.rm=T),
                                               by=c("projection", "country")]
-pred_age_2023$stat <- rep(c("Q2.5", "Q50", "Q97.5"), nrow(pred_age_2023)/3)
-pred_age_2023 <- dcast.data.table(pred_age_2023,country + projection~stat, value.var = "V1")
-
-#add in teh actual 2023 data
-colnames(validation_data_R)[which(colnames(validation_data)=="RegionCode")] <- "country"
-pred_age_2023[validation_data_R, on = "country", num := i.NumValue]
-pred_age_2023[coverage_23, on = c(country = "country_short"), cov := i.Coverage]
-
-pred_age_2023[, num := as.numeric(num)]
-pred_age_2023[, cov := as.numeric(cov)]
-pred_age_2023[, act_num := num/(cov/100)]
-
-if(fixed_incidence == T){
-  pred_age_2023_T <- pred_age_2023
-  pred_age_2023_T$type = "fixed"
-}
-
-if(fixed_incidence == F){
-  pred_age_2023_F <- pred_age_2023
-  pred_age_2023_F$type = "changing"
-}
-  
+    pred_age_2023$stat <- rep(c("Q2.5", "Q50", "Q97.5"), nrow(pred_age_2023)/3)
+    pred_age_2023 <- dcast.data.table(pred_age_2023,country + projection~stat, value.var = "V1")
+    
+    #add in teh actual 2023 data
+    colnames(validation_data_R)[which(colnames(validation_data)=="RegionCode")] <- "country"
+    pred_age_2023[validation_data_R, on = "country", num := i.NumValue]
+    pred_age_2023[coverage_23, on = c(country = "country_short"), cov := i.Coverage]
+    
+    pred_age_2023[, num := as.numeric(num)]
+    pred_age_2023[, cov := as.numeric(cov)]
+    pred_age_2023[, act_num := num/(cov/100)]
+    
+    if(fixed_incidence == T){
+      pred_age_2023_T <- pred_age_2023
+      pred_age_2023_T$type = "fixed"
+    }
+    
+    if(fixed_incidence == F){
+      pred_age_2023_F <- pred_age_2023
+      pred_age_2023_F$type = "changing"
+    }
+    
   }
   combo <- rbind(pred_age_2023_T, pred_age_2023_F)
-
-VALIDATION_LOG <-  ggplot(combo[projection == "BSL"], aes(x=country, y = Q50)) + 
-  geom_pointrange(aes(ymin =Q2.5, ymax=Q97.5, colour = type, fill = type), position = position_dodge(width=0.5)) + 
-  facet_grid(projection~.) + 
-  coord_flip() + 
-  scale_y_log10() + 
-  geom_point(aes(y = act_num), colour = "black") + 
-  labs(title = paste0("2023: ", bug_choice))
-
-VALIDATION <-  ggplot(combo[projection == "BSL"], aes(x=country, y = Q50)) + 
-  geom_pointrange(aes(ymin =Q2.5, ymax=Q97.5, colour = type, fill = type), position = position_dodge(width=0.5)) + 
-  facet_grid(projection~.) + 
-  coord_flip() + 
-  geom_point(aes(y = act_num), colour = "black") + 
-  labs(title = paste0("2023: ", bug_choice))
-
-#check if even or not, so can get two plots per figure
-if(i %% 2 == 0){
   
-  ggsave(grid.arrange( VALIDATION_1, VALIDATION, nrow =1), 
-         file=paste0("plots_nw/validation_inc_",bug_choice,".pdf"),
-         width =9, height = 5)
+  VALIDATION_LOG <-  ggplot(combo[projection == "BSL"], aes(x=country, y = Q50)) + 
+    geom_pointrange(aes(ymin =Q2.5, ymax=Q97.5, colour = type, fill = type), position = position_dodge(width=0.5)) + 
+    facet_grid(projection~.) + 
+    coord_flip() + 
+    scale_y_log10() + 
+    geom_point(aes(y = act_num), colour = "black") + 
+    labs(title = paste0("2023: ", bug_choice))
   
-}   else {
+  VALIDATION <-  ggplot(combo[projection == "BSL"], aes(x=country, y = Q50)) + 
+    geom_pointrange(aes(ymin =Q2.5, ymax=Q97.5, colour = type, fill = type), position = position_dodge(width=0.5)) + 
+    facet_grid(projection~.) + 
+    coord_flip() + 
+    geom_point(aes(y = act_num), colour = "black") + 
+    labs(title = paste0("2023: ", bug_choice))
   
-  VALIDATION_1 <- VALIDATION
+  #check if even or not, so can get two plots per figure
+  if(i %% 2 == 0){
+    
+    ggsave(grid.arrange( VALIDATION_1, VALIDATION, nrow =1), 
+           file=paste0("plots_nw/validation_inc_",bug_choice,".pdf"),
+           width =9, height = 5)
+    
+  }   else {
+    
+    VALIDATION_1 <- VALIDATION
+  }
+  
+  
+  combo$bug <- bug_choice
+  store_comparison <- rbind(store_comparison, combo)
+  
+  # ##### COMPARE INCIDENCE #####
+  # # validation against total tests
+  # validation_data_T <- validation_data[Indicator== "Total tested isolates" & 
+  #                                        Population == bug_choice]
+  # 
+  # 
+  # tot_incidence_23[coverage_23, on = c(country = "country_short"), cov := i.Coverage]
+  # tot_incidence_23[, estimated := V1/(cov/100)]
+  # # Subset the relevant pathogen. 
+  # if(length(grep(plain_model_names[model_target], pattern = "esccol"))==1){
+  #   inc_comp <-tot_incidence_23[pathogen == "Escherichia coli"] 
+  #   
+  # } else if(length(grep(plain_model_names[model_target], pattern = "acispp"))==1){
+  #   inc_comp <-tot_incidence_23[pathogen == "Acinetobacter spp"]
+  #   
+  # } else if(length(grep(plain_model_names[model_target], pattern = "staaur"))==1){
+  #   inc_comp <-tot_incidence_23[pathogen == "Staphylococcus aureus"]
+  #   
+  # } else if(length(grep(plain_model_names[model_target], pattern = "strpne"))==1){
+  #   inc_comp <-tot_incidence_23[pathogen == "Streptococcus pneumoniae"]
+  #   
+  # } else if(length(grep(plain_model_names[model_target], pattern = "encfai"))==1){
+  #   inc_comp <-tot_incidence_23[pathogen == "Enterococcus faecalis"]
+  #   
+  # } else if(length(grep(plain_model_names[model_target], pattern = "encfae"))==1){
+  #   inc_comp <-tot_incidence_23[pathogen == "Enterococcus faecium"]
+  #   
+  # } else if(length(grep(plain_model_names[model_target], pattern = "klepne"))==1){
+  #   inc_comp <-tot_incidence_23[pathogen == "Klebsiella pneumoniae"]
+  #   
+  # } else if(length(grep(plain_model_names[model_target], pattern = "pseaer"))==1){
+  #   inc_comp <-tot_incidence_23[pathogen == "Pseudomonas aeruginosa"]
+  # }
+  # 
+  # inc_comp[validation_data_T, on =c(country = "RegionCode"), validation := i.NumValue]
+  # inc_comp[, validation := as.numeric(validation)]
+  # 
+  # 
+  # inc_comp_m <- melt.data.table(inc_comp, id.vars = c("country", "pathogen", "V1", "cov"))
+  # 
+  # VALIDATION_INCIDENCE <- ggplot(inc_comp_m, aes(x=country, y = value, colour = variable, group = country)) + 
+  #   geom_point() +  
+  #   scale_y_log10()+
+  #   geom_line(linetype= "dotted", colour = "black") +
+  #   coord_flip() + 
+  #   scale_color_manual(values = c("black", "red"))+
+  #   labs(title = paste0(bug_choice, ": incidence validation"))
+  
+  
 }
 
 
-combo$bug <- bug_choice
-store_comparison <- rbind(store_comparison, combo)
+store_comparison <- data.table(store_comparison)
+store_comparison[, percent_dif := ((act_num-Q50)/act_num)*100]
+subset_to_validate <- store_comparison[projection =="BSL" &
+                                         type == "changing" &
+                                         !is.na(percent_dif), ]
 
-# ##### COMPARE INCIDENCE #####
-# # validation against total tests
-# validation_data_T <- validation_data[Indicator== "Total tested isolates" & 
-#                                        Population == bug_choice]
-# 
-# 
-# tot_incidence_23[coverage_23, on = c(country = "country_short"), cov := i.Coverage]
-# tot_incidence_23[, estimated := V1/(cov/100)]
-# # Subset the relevant pathogen. 
-# if(length(grep(plain_model_names[model_target], pattern = "esccol"))==1){
-#   inc_comp <-tot_incidence_23[pathogen == "Escherichia coli"] 
-#   
-# } else if(length(grep(plain_model_names[model_target], pattern = "acispp"))==1){
-#   inc_comp <-tot_incidence_23[pathogen == "Acinetobacter spp"]
-#   
-# } else if(length(grep(plain_model_names[model_target], pattern = "staaur"))==1){
-#   inc_comp <-tot_incidence_23[pathogen == "Staphylococcus aureus"]
-#   
-# } else if(length(grep(plain_model_names[model_target], pattern = "strpne"))==1){
-#   inc_comp <-tot_incidence_23[pathogen == "Streptococcus pneumoniae"]
-#   
-# } else if(length(grep(plain_model_names[model_target], pattern = "encfai"))==1){
-#   inc_comp <-tot_incidence_23[pathogen == "Enterococcus faecalis"]
-#   
-# } else if(length(grep(plain_model_names[model_target], pattern = "encfae"))==1){
-#   inc_comp <-tot_incidence_23[pathogen == "Enterococcus faecium"]
-#   
-# } else if(length(grep(plain_model_names[model_target], pattern = "klepne"))==1){
-#   inc_comp <-tot_incidence_23[pathogen == "Klebsiella pneumoniae"]
-#   
-# } else if(length(grep(plain_model_names[model_target], pattern = "pseaer"))==1){
-#   inc_comp <-tot_incidence_23[pathogen == "Pseudomonas aeruginosa"]
-# }
-# 
-# inc_comp[validation_data_T, on =c(country = "RegionCode"), validation := i.NumValue]
-# inc_comp[, validation := as.numeric(validation)]
-# 
-# 
-# inc_comp_m <- melt.data.table(inc_comp, id.vars = c("country", "pathogen", "V1", "cov"))
-# 
-# VALIDATION_INCIDENCE <- ggplot(inc_comp_m, aes(x=country, y = value, colour = variable, group = country)) + 
-#   geom_point() +  
-#   scale_y_log10()+
-#   geom_line(linetype= "dotted", colour = "black") +
-#   coord_flip() + 
-#   scale_color_manual(values = c("black", "red"))+
-#   labs(title = paste0(bug_choice, ": incidence validation"))
+subset_to_validate[, log_error :=log(Q50/act_num)]
 
 
-  }
-
-  
-  store_comparison <- data.table(store_comparison)
-  store_comparison[, percent_dif := ((act_num-Q50)/act_num)*100]
-  subset_to_validate <- store_comparison[projection =="BSL" &
-                                           type == "changing" &
-                                           !is.na(percent_dif), ]
-
-  subset_to_validate[, log_error :=log(Q50/act_num)]
-
-  
-  LOGERROR <- ggplot(subset_to_validate,
-         aes(x = log_error, fill = country)) + geom_histogram() + 
-geom_vline(xintercept = 0.7, linetype = "dashed") + 
-    geom_vline(xintercept = -0.7, linetype = "dashed") + 
-   # lims(x = c(0,250)) + 
-    labs(x ="Log error")
-  ggsave(grid.arrange(LOGERROR, nrow =1), file=paste0("plots_nw/logerror.pdf"),
-         width =9, height = 5)
-    
+LOGERROR <- ggplot(subset_to_validate,
+                   aes(x = log_error, fill = country)) + geom_histogram() + 
+  geom_vline(xintercept = 0.7, linetype = "dashed") + 
+  geom_vline(xintercept = -0.7, linetype = "dashed") + 
+  # lims(x = c(0,250)) + 
+  labs(x ="Log error")
+ggsave(grid.arrange(LOGERROR, nrow =1), file=paste0("plots_nw/logerror.pdf"),
+       width =9, height = 5)
